c53b7d4a6ce8c4e42a69b9f6eeb3c216926214b5
SIKANDAR SINGH <sikandarsingh96@gmail.com>
2020-04-10 22:22:57 +0530

    TL- Created Employee class with empNo and empName field

---
+++ TetApp/src/com/sik/Student.java
@@ -0,0 +1,28 @@
+package com.sik;
+
+public class Student {
+
+	private Integer empId;
+	private String empName;
+
+	public Integer getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(Integer empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [empId=" + empId + ", empName=" + empName + "]";
+	}
+}
